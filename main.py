46c634ad19411d4136dc20364404350482d695ce
anukaal <mailanu98@gmail.com>
2021-07-18 10:11:11 +0530

    adding conversation

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
 bot = ChatBot("My Bot")
+
+conv = [
+    'hello',
+    'Hi there',
+    'What is your name?',
+    'My name is bot , made by anukaal',
+    'How are you?',
+    'I am good',
+    'Thanku',
+    'In which city do you live',
+    'I live in Bokaro steel city'
+
+]
